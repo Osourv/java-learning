498448b52ac6eab92cad4b356be955e91787adef
suraj8800 <biswassuraj205@gmail.com>
2025-07-23 21:27:24 +0530

    sumof2numbersInput

--- javainput/sumof2numbersInput.java
+++ javainput/sumof2numbersInput.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class sumof2numbersInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+      // in java the code run line by line 
         System.out.print("enter the first number :");
         int x = sc.nextInt();
 
