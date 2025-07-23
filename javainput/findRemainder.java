5c58bca41b28ee8e8838ef810e296c2d548cfc78
suraj8800 <biswassuraj205@gmail.com>
2025-07-23 21:54:52 +0530

    find remainder

---
+++ javainput/findRemainder.java
@@ -0,0 +1,19 @@
+package javainput;
+import java.util.Scanner;
+public class findRemainder {
+    public static void main(String[] args) {
+        //take two integers input,a and b : a>b,and find the reaminder when a is divided by b.
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("take 1st int : ");
+        int a = sc.nextInt();
+
+        System.out.print("take 2nd int: ");
+        int b = sc.nextInt();
+        
+        int rem = a % b;
+        System.out.println("remainder: " + rem);
+        
+    }
+    
+}
