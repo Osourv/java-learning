5d1b3c4b471251510084a240462f996a362d88eb
suraj8800 <biswassuraj205@gmail.com>
2025-07-24 09:39:16 +0530

    character datatype

---
+++ javainput/chardata/chardatatype.java
@@ -0,0 +1,10 @@
+package javainput.chardata;
+import java.util.Scanner;
+public class chardatatype {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char ch = 'a';
+        System.out.println(ch);
+    }
+}
