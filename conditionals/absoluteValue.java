549058263d59b765d60089659edf18a3b25f181f
suraj8800 <biswassuraj205@gmail.com>
2025-07-30 21:38:56 +0530

    absolute value of the integer

---
+++ conditionals/absoluteValue.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class absoluteValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter any integer :");
+
+        int n = sc.nextInt();
+        if(n<0){  //if n is less than zero
+
+            n = n * (-1); // (-4)*(-1) = 4 positive interger |-4| = 4 
+        }
+            System.out.println("the absolute value is: " +n);
+        }
+
+
+    }
