96801aa86044bf9297ca2b5c3292efca9246d8b6
suraj8800 <biswassuraj205@gmail.com>
2025-07-31 20:12:22 +0530

    take input percentage of a students and print the grade according to marks.

---
+++ conditionals/percentageElseif.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class percentageElseif {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("enter the percentage: ");
+
+        double n = sc.nextDouble();
+
+        if(n>=90){
+            System.out.println("excellent");
+        }
+        else if(n>=80){
+            System.out.println("very good");
+        }
+        else if(n>=70){
+        System.out.println("good");
+        }
+        else if(n>=60){
+            System.out.println("can do better");
+        }
+        else if(n>=50){
+            System.out.println("average");
+        }
+        else if(n<=50){
+            System.out.println("below average");
+        }
+        else{
+            System.out.println("fail");
+        }
+    }
+}
