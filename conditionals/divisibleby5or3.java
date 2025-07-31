2aab768fc5e4dfc6cb75b374bcb14bb670b661a8
suraj8800 <biswassuraj205@gmail.com>
2025-07-31 20:40:10 +0530

    take positive integer input and tell if it is divisible by 5 or 3.

---
+++ conditionals/divisibleby5or3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class divisibleby5or3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter a number: ");
+        int n = sc.nextInt();
+
+        if(n%5==0 && n%3==0){
+            System.out.println("this is divisible by 5or3: ");
+        }
+        else{
+            System.out.println("this is not divisible: ");
+        }
+
+
+    }
+}
