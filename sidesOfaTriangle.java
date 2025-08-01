f5a3f7e100b312c36c043cecc2c89bb4330e078a
suraj8800 <biswassuraj205@gmail.com>
2025-08-01 19:12:22 +0530

    take 3 numbers input and tell if they can be the sides of a triangle.

---
+++ sidesOfaTriangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class sidesOfaTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter first side: ");
+        int a = sc.nextInt();
+
+        System.out.println("enter the second side: ");
+        int b = sc.nextInt();
+
+        System.out.println("enter the third side: ");
+        int c = sc.nextInt();
+
+        if(a+b>c && b+c>a && c+a>b){
+            System.out.println("all sides are of a triangle ");
+        }
+        else{
+            System.out.println("invalid triangle");
+        }
+    }
+}
