96b19e6ed9e13d75280527bb9ead7637ddaf40d4
suraj8800 <biswassuraj205@gmail.com>
2025-08-07 17:40:18 +0530

    q->2 (print 1 to 100)

--- loops/A_BasicOfLoops.java
+++ loops/A_BasicOfLoops.java
@@ -1,14 +1,14 @@
 package loops;
 import java.util.Scanner;
-public class A_hello {
+public class A_BasicOfLoops {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // System.out.println("enter the input: ");
-        // int x = sc.nextInt();
+        System.out.println("enter the input: ");
+        int x = sc.nextInt();
 
 
-        for(int i = 1; i<3; i++){
+        for(int i = 1; i<=x; i++){
             System.out.println("hello world");
         }
     }
---
+++ loops/B_printOnetoTen.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class B_printNumber {
+    public static void main(String[] args) {
+        int i;
+        for( i=1; i<=100; i++){
+            System.out.println(i);
+        }
+        System.out.println("last whhen i is smaller or equal to 100: "+i);
+        
+    }
+}
