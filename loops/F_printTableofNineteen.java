1302e279de5ec1e02506c094ce9182244a62a852
suraj8800 <biswassuraj205@gmail.com>
2025-08-07 18:38:55 +0530

    q6->print the table of 19.

---
+++ loops/F_printTableofNineteen.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class F_printTableofNineteen {
+    public static void main(String[] args) {
+       // ye loop 117 times chal rha hai
+        // for(int i=19;i<=190; i++){
+        //     if(i%19==0){
+        //         System.out.println(i);
+
+         for(int i=19;i<=190; i+=19){
+         System.out.println(i);
+        }
+    }
+}
