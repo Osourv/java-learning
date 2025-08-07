f43e3f1c64ec68023fd0e5e619261d122c3875ae
suraj8800 <biswassuraj205@gmail.com>
2025-08-07 19:47:38 +0530

    q8->disaplay ap upto n terms using formula and without formula.

---
+++ loops/I_arithmeticProgression.java
@@ -0,0 +1,24 @@
+package loops;
+import java.util.Scanner;
+public class I_arithmeticProgression {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter a number: ");
+        int n = sc.nextInt();
+
+        //1 3 5 7 9    2n-1
+        for(int i = 1; i<=2*n-1; i+=2){
+            System.out.println(i+" ");
+        }
+
+        //method 2 -> 3,7 ,11, 15 .....
+        int a = 3  , d = 4;
+        for(int i = 1; i<=n; i++){
+            System.out.println(a+" ");
+            a += d;
+        } 
+
+        
+    }
+}
