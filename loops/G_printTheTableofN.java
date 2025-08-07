7001bf4c25921a3094fb4370bcc1cd497be69fd2
suraj8800 <biswassuraj205@gmail.com>
2025-08-07 18:58:15 +0530

    q7-> print the table of n. heren is a integer which user will input.

--- loops/G_printTheTableofN.java
+++ loops/G_printTheTableofN.java
@@ -3,7 +3,20 @@ import java.util.Scanner;
 public class G_printTheTableofN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt():
-        System.out.println("enter thhe table");
+        System.out.println("enter the table number: ");
+         int n = sc.nextInt();
+        // using if-else
+        //  for(int i=1;i<=10; i++){
+        //     if(i<=10){
+        //         System.out.println(n*i);
+        //     }
+        //  }
+        
+
+        //using increment changes
+        for(int i=1;i<=10; i+=1){
+         System.out.println(n*i);
+
     }
+}
 }
