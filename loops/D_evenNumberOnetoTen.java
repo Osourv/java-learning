fbc814d0ac4ad3567b755a1fd8094846df6046c7
suraj8800 <biswassuraj205@gmail.com>
2025-08-07 19:20:56 +0530

    q4->print all the even/odd number from one to 100.

--- loops/D_evenNumberOnetoTen.java
+++ loops/D_evenNumberOnetoTen.java
@@ -2,15 +2,23 @@ package loops;
 
 public class D_evenNumberOnetoTen {
     public static void main(String[] args) {
+      
 
-      for(int i=1; i<=100; i++){
-        if(i%2!=0){
+
+      //100 bar loop chal rha hai
+      for(int i=2; i<=100; i++){
+        if(i%2==0){
             System.out.println(i);
         }
         
-        }
+      }
+      //50 bar loop chal rha hai and 1 change can give even or odd int i =1,2 give even or odd
+        for(int i=1; i<=100; i+=2){
+            System.out.println(i);
       }
         
         
     }
+  }
+
 
