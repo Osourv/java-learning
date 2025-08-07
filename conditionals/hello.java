c40b35727ebd1d988cf13d05336654bf83d40bb8
suraj8800 <biswassuraj205@gmail.com>
2025-08-07 17:11:59 +0530

    1st question->  basic of loops

---
+++ conditionals/hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        boolean c;
+        int a = 5,b;
+        b = a = 15;
+        c = a< 15;
+        System.out.println("h");
+    }
+}
---
+++ loops/A_BasicOfLoops.java
@@ -0,0 +1,16 @@
+package loops;
+import java.util.Scanner;
+public class A_hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("enter the input: ");
+        // int x = sc.nextInt();
+
+
+        for(int i = 1; i<3; i++){
+            System.out.println("hello world");
+        }
+    }
+    
+}
