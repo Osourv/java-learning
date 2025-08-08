29387be60b30164fc81f3d31b611ae6f011f6b84
suraj8800 <biswassuraj205@gmail.com>
2025-08-08 20:59:58 +0530

    q14-> wap to print only odd number using continue statement.

---
+++ loops/N_oddnumber.java
@@ -0,0 +1,17 @@
+package loops;
+import java.util.Scanner;
+public class N_oddnumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter a number: ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=100; i++){
+            if(i%2==0)continue;
+            System.out.println(i);
+            
+        }
+        
+    }
+}
