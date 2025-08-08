cdc426ac353000eb03d4ad78911b929425166e5e
suraj8800 <biswassuraj205@gmail.com>
2025-08-08 22:15:30 +0530

    q16->predit the output.

---
+++ loops/P_predictOutput.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class P_predictOutput {
+    public static void main(String[] args) {
+        
+    
+    int x = 4, y = 0 ;
+    while(x>=0){
+        x--;
+        y++;
+        if(x == y) continue;
+        else System.out.println(x+" "+y);
+    }
+
+}
+}
+
+
