7e8448d27fc83ab232c0f5585aded4679dcff448
suraj8800 <biswassuraj205@gmail.com>
2025-08-26 20:31:18 +0530

    q22->sum of number +reverse.

---
+++ loops/W_SumaddRevofAnumber.java
@@ -0,0 +1,22 @@
+package loops;
+import java.util.Scanner;
+public class W_SumaddRevofAnumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int n = sc.nextInt();
+        int sum = n,rev = 0;
+
+        while(n!=0){
+            int ld=n%10;
+            rev= rev*10+ld;
+            n/=10;
+            
+            
+
+        }
+        sum= sum+rev;
+        System.out.println("sum+rev : " +sum );
+        System.out.println("sum+rev : " +rev );
+    }
+}
