34a70d2abab895b5b85334d16647e4f72afeb2e9
Osourv <souravvishwas4@gmail.com>
2025-09-01 10:44:33 +0530

    q4->print alphabet square.

---
+++ Pattern_printing/D_alphabetSquare.java
@@ -0,0 +1,17 @@
+package Pattern_printing;
+import java.util.Scanner;
+public class D_alphabetSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print((char)(j+64)+" ");//isme number ko ascii value mai change kiya hai (typecast)
+            }
+            System.out.println();
+        }
+
+    }
+}
