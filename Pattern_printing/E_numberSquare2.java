aa9e5a822fc47e4c39e896de6876182e7e53c2ff
Osourv <souravvishwas4@gmail.com>
2025-09-01 11:34:02 +0530

    q5->print number square 2.

--- Pattern_printing/E_numberSquare2.java
+++ Pattern_printing/E_numberSquare2.java
@@ -1,17 +1,23 @@
 package Pattern_printing;
 import java.util.Scanner;
-public class D_alphabetSquare {
+public class E_numberSquare2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a number: ");
         int n = sc.nextInt();
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                System.out.print((char)(j+64)+" ");// important ->isme number ko ascii value mai change kiya hai (typecast)
+        for(int i = 1; i<=n; i++){
+            for(int j= 1; j<=n; j++){
+                System.out.print(i);
             }
             System.out.println();
         }
-
     }
 }
+//output
+// enter a number: 
+// 4
+// 1111
+// 2222
+// 3333
+// 4444
