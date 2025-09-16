1b42e04058ce7f28e5e2dd54b1467d92c2e282ac
Osourv <souravvishwas4@gmail.com>
2025-09-16 18:29:25 +0530

    java
    Q->10.

--- Pattern_printing/special pattern/J_alphabetTriangleVerticallyFlipped.java
+++ Pattern_printing/special pattern/J_alphabetTriangleVerticallyFlipped.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-public class I_numbertriagle2 {
+public class J_alphabetTriangleVerticallyFlipped {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
 
         for(int i = 1; i<=n; i++){
             for(int j = 1; j<=n-i; j++){
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for(int j = 1; j<=i; j++){
-                System.out.print(j);
+                System.out.print((char)(i+64)+" ");
             }
             System.out.println();
         }
+
     }
 }
