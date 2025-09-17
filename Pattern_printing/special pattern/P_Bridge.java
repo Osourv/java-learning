6a5a26370b27ac70d5d3ecbc6b0766293850f938
Osourv <souravvishwas4@gmail.com>
2025-09-17 19:38:32 +0530

    Update P_Bridge.java

--- Pattern_printing/special pattern/P_Bridge.java
+++ Pattern_printing/special pattern/P_Bridge.java
@@ -4,19 +4,19 @@ public class P_Bridge {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=1; i<=2*n-1;i++){
+        for(int i=1; i<=2*n-1;i++){//for upper most stars
         System.out.print("* ");
         }
         System.out.println();
          int nsp= 1;
-         for(int i=1; i<=n-1;i++){
+         for(int i=1; i<=n-1;i++){//first triangle stars
             for(int j= 1; j<=n-i; j++){
                 System.out.print("* ");
             }
-            for(int j=1; j<=nsp;j++){
+            for(int j=1; j<=nsp;j++){//spaces
                 System.out.print("  ");
             }
-                for(int j =1;j<=n-i; j++){
+                for(int j =1;j<=n-i; j++){// second triangle spaces
                     System.out.print("* ");
                 }
                 nsp+=2;
