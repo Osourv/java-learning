646ddb56cfa3866dd4ab139a1df4dd9243e4e611
Osourv <souravvishwas4@gmail.com>
2025-09-29 18:52:05 +0530

    Create C_syntax.java
    q->3

---
+++ Methods/C_syntax.java
@@ -0,0 +1,32 @@
+package Methods;
+
+public class C_syntax {
+//     public static void sourav() {
+//         System.out.println("world!");
+        
+//     }
+//     public static void main(String[] args) {
+//         System.out.println("hello");
+//         sourav();
+//         suraj();
+//     }
+//     public static void suraj() {
+//         System.out.println("headphone");
+        
+//     }
+// }
+public static void main(String[] args) {
+    sapna();
+}
+public static void sourav() {
+    suraj();
+    System.out.println("samsung");
+}
+public static void suraj() {
+    System.out.println("hello world!");
+}
+public static void sapna() {
+    System.out.println("powerbank");
+    sourav();
+}
+}
