c074f63d77e71d51152646257b7789c6ecea4a0d
Osourv <souravvishwas4@gmail.com>
2025-09-29 18:03:41 +0530

    Update A_Builtin.java

--- Methods/A_Builtin.java
+++ Methods/A_Builtin.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 
 public class A_Builtin {
     public static void main(String[] args) {
-        System.out.println(Math.sqrt(100));
-        System.out.println();
+        // System.out.println(Math.sqrt(100)); //squareroot
+        // System.out.println(Math.cbrt(100)); //cube root
+        // System.out.println(Math.floor(3.7)); // greatest integer function
+        // System.out.println(Math.ceil(6.9)); // ceiling value
+        // System.out.println(Math.min(10, 2)); // minimum 
+        // System.out.println(Math.max(2, 6)); // maximum
     }
 }
