98e8269e8ce520daedcdf3b5c1027b4a36f04459
Osourv <souravvishwas4@gmail.com>
2025-09-29 20:57:54 +0530

    Update D_arguments.java

--- Methods/D_arguments.java
+++ Methods/D_arguments.java
@@ -1,12 +1,13 @@
 package Methods;
 
 public class D_arguments {
-    public static void max(int a, int b, int c) {
+    public static void max(int a, int b, int c) {//parameter
         if(a>=b && a>=c)System.out.println(a);
         else if(b>=a && b>=c)System.out.println(b);
         else System.out.println(c);
     }
     public static void main(String[] args) {
-        max(4,5,4);
+        max(4,5,4);//arguments
     }
 }
+ 
