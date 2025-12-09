725dbe818b96b90d991a5b1cac098cc9805d2c43
Osourv <souravvishwas4@gmail.com>
2025-12-09 18:47:04 +0530

    Create A_ArrayBasics.java

---
+++ Arrays/A_ArrayBasics.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class A_ArrayBasics {
+    public static void main(String[] args) {
+      //  int[] arr = {6,19,12,5,765,4};
+      //    //indexing
+      //    System.out.println(arr[3]); //access
+      //    //Updating elements
+      //    arr[3] = 87;
+      //    System.out.println(arr[3]);
+
+         int [] arr = new int[5]; // 5 size ka array | 0 to 4
+         //this is initialization
+         arr[0] = -10;
+         arr[1] = 1;
+         arr[2] = 104;
+         arr[3] = 110;
+         arr[4] = 30;
+         
+
+    }
+}
