60d0c0c495824630381fb69f00b483d1b97aa25c
Osourv <souravvishwas4@gmail.com>
2025-12-09 20:13:34 +0530

    Create C_PrintNegative.java

---
+++ Arrays/C_PrintNegative.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class C_PrintNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter array size :");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.print("enter array elements:");
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+
+            //printing negative values
+            for(int i=0; i<n; i++){
+                if(arr[i]<0) 
+                    System.out.print(arr[i]+" ");
+            }
+        }
+        
+    }
+
